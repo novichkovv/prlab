b6dbf666009375ef1c24c57d98422c01662479e6
novichkovv <novichkovv@bk.ru>
2015-12-02 00:17:29 +0300

    1
    Signed-off-by: novichkovv <novichkovv@bk.ru>

--- first_dump.sql
+++ first_dump.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `plab` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `plab`;
+CREATE DATABASE  IF NOT EXISTS `prlab` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `prlab`;
 -- MySQL dump 10.13  Distrib 5.6.23, for Win32 (x86)
 --
 -- Host: 127.0.0.1    Database: plab
